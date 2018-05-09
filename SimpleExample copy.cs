ade311cee4c3c8874899eb8bca76bf72c4694007
KieliAdkins <39067815+KieliAdkins@users.noreply.github.com>
2018-05-09 12:48:46 -0700

    Add files via upload
    First commit for Unity project.

---
+++ SimpleExample copy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleExample : MonoBehaviour {
+
+    public string theText = "Hello World";
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Write the value stored in our variable "theText" to the console window
+        Debug.Log(theText);
+	}
+}
